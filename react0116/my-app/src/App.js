cfaee3cc82b2980248b8200f2f81b2d6175651fe
jyy <kanda8@nate.com>
2024-01-16 13:29:37 +0900

    useState 사용법 -첫번쨰배열은 읽기 두번째배열은 업데이트

--- react0116/my-app/src/App.js
+++ react0116/my-app/src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import Home from './pages/Home.js';
 import About from './pages/About.js';
 import {Routes,Route,Link} from 'react-router-dom';
+import Counter from './pages/Counter.js';
+import Input from './pages/Input.js';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> {/* a태그와 같은 Link태그 */ }
-        <Link to="/about">About</Link>
+        <Link to="/">Home</Link> {/* a태그와 같은 Link태그 */ }|
+        <Link to="/about">About</Link>|
+        <Link to="/counter">Counter</Link>
+        <Link to="/input">Input</Link>
       </nav>
       <Routes>   {/* Router -> 매핑  Routes안에 Route로 세팅  Routes가 Link와 매핑되며 페이지 변경*/}
         <Route path='/' element={<Home />} />    {/* to와 path 동기화  */}
         <Route path='/about' element={<About />} />
+        <Route path='/counter' element={<Counter />} />
+        <Route path='/input' element={<Input />} />
       </Routes>
+      
     </div>
   );
 }
