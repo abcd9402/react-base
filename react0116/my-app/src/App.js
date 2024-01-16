78f0bdfa39081d2c3cbe9c0db929fa28cc58139d
jyy <kanda8@nate.com>
2024-01-16 12:51:22 +0900

    - 부분 컴포넌트 매핑관련 Router import - a태그와 같은 Link태그 사용으로 페이지 이동

--- react0116/my-app/src/App.js
+++ react0116/my-app/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Home from './pages/Home.js';
+import {Routes,Route,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">about</Link>
+      </nav>
       <Home />
     </div>
   );
