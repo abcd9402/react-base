6fd355900c6361f822bc7d85b4b44664363b3755
jyy <kanda8@nate.com>
2024-01-16 12:19:39 +0900

    class 컴포넌트 방식 생성

--- react0116/my-app/src/pages/Home.js
+++ react0116/my-app/src/pages/Home.js
@@ -1,7 +1,21 @@
-import React from 'reat';
+// 함수형 컴포넌트 방식
+// import React from 'reat';
 
-function Home(){
-    return <h1>Home 화면 입니다.</h1>
+// function Home(){
+//     return <h1>Home 화면 입니다.</h1>
+// }
+
+// export default Home;
+
+
+//클래스 컴포넌트 방식
+import React,{Component} from 'react';
+
+class Home extends Component{
+    render() {
+        return <h1>Home 화면 입니다.</h1>
+    }
 }
+
 
 export default Home;
