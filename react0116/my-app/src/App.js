2404e16709eeed6b8b8762c5ca84c2a6cccad775
jyy <kanda8@nate.com>
2024-01-16 13:02:05 +0900

    Router,Link를 이용한 페이지 매핑

--- react0116/my-app/src/App.js
+++ react0116/my-app/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Home from './pages/Home.js';
+import About from './pages/About.js';
 import {Routes,Route,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">about</Link>
+        <Link to="/">Home</Link> {/* a태그와 같은 Link태그 */ }
+        <Link to="/about">About</Link>
       </nav>
-      <Home />
+      <Routes>   {/* Router -> 매핑  Routes안에 Route로 세팅  Routes가 Link와 매핑되며 페이지 변경*/}
+        <Route path='/' element={<Home />} />    {/* to와 path 동기화  */}
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
