bedeb04fdb7a0e8028f778677ca5969c74150a8b
jyy <kanda8@nate.com>
2024-01-17 17:29:34 +0900

    변수,css,sytle -jsx 내에서 는 {변수명} 으로 사용 -css파일을 상단에 import하여 적용 가능 class -> className으로 변경 -style 사용 시 {}안에 오브젝트 형식으로 작성 및 -기호는 사용불가 하며 카멜형식으로 입력 ex){{color='red',fontSize='16px'}}

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">  {/*class는 jsx에서 calssName으로 선언*/}
+        <h4 id={post}  style={{color:'red', fontSize:'16px' }} >블로그임</h4>  {/* style입력시 {}안에 오브젝트형식으로 작성 '-'기호는 불가 -> 카멜형식 */}
+      </div>
+      <h4>{post}</h4>  {/* 변수의 데이터를 jsx에 입력시 {}안에 사용   ->  데이터 바인딩  */}
     </div>
   );
 }
