d41b4ee75936a6921cc9fc479558298bed97ff97
jyy <kanda8@nate.com>
2024-01-17 18:13:50 +0900

    useState변경, onClick이벤트 -useState는 두번쨰인덱스로 변경가능 -이벤트는 함수로 받을 수 있다

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,11 +6,18 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목,글제목수정] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);  // useState 만드는것 -> 앞에는 읽기전용, 뒤에는 업데이트전용 -> 변수변경시 -> 뒤의변수('변경할내용')
                                                       // 재랜더링이 필요할시(동적) useState사용 정적인 변수는 일반적인 방식으로 사용
-    let num= [1,2];
+  let num= [1,2];
 //  let a = num[0];
 //  let b = num[1];
-    let [a,b] = num;   //Distruct 기법
+  let [a,b] = num;   //Distruct 기법
 
+  let [따봉,따봉변경] = useState(0);
+
+
+  const goodUp = ()=>{  //글제목[0] 옆 따봉 클릭시 실행되는 이벤트
+    // 따봉 = 따봉 + 1      -> 불가능 
+    따봉변경(따봉+1);
+  }
 
   return (
     <div className="App">
@@ -18,7 +25,11 @@ function App() {
         <h4 id={post}  style={{color:'red', fontSize:'16px' }} >블로그임</h4>  {/* style입력시 {}안에 오브젝트형식으로 작성 '-'기호는 불가 -> 카멜형식 */}
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>  {/* 변수의 데이터를 jsx에 입력시 {}안에 사용   ->  데이터 바인딩  */}
+        <h4>{글제목[0]} <span onClick={goodUp}>👍</span > {따봉} </h4>  {/* 변수의 데이터를 jsx에 입력시 {}안에 사용   ->  데이터 바인딩  */}
+                                    {/* onClick={함수}로 이벤트 작성  */}
+        <button onClick={()=>{
+          글제목수정(['여자 코트 추천','강남 우동 맛집','파이썬독학']);
+        }}>제목 변경</button>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
